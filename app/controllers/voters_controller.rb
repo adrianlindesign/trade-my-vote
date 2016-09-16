577789311391ef9b2e3f35185825398726e2427a
AdrianMLin <adrian.mh.lin@gmail.com>
2016-09-16 18:54:55 -0400

    new voters succesfully added to database

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -3,12 +3,16 @@ class VotersController < ApplicationController
     new_voter = Voter.create(
       email: params["email"],
       user_state: params["user_state"],
-      desired_candidate: params["desired_candidate"]
+      desired_candidate: params["desired_candidate"],
+      paired: false
     )
+
+    redirect_to '/'
   end
 
   def match
     #match the voters
+    #check if there are any unpaired ones
   end
 
 
