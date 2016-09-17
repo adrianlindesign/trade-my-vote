14491f0aaaf58f3ac2885f70ba8188b641cf9575
AdrianMLin <adrian.mh.lin@gmail.com>
2016-09-17 01:56:07 -0400

    working on matching algorithm

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -11,9 +11,73 @@ class VotersController < ApplicationController
   end
 
   def match
-    #match the voters
-    #check if there are any unpaired ones
+
+    #variables
+    swing_states = [
+      "AZ",
+      "CO",
+      "FL",
+      "GA",
+      "IA",
+      "NC",
+      "NH",
+      "NM",
+      "NV",
+      "OH",
+      "PA",
+      "VA",
+      "WI"
+    ]  
+    unpaired_voters = Voter.where(paired: false);
+
+    #arrays 
+    voter_pairs = []
+
+    swing_state_3rd_party_voters = []
+    solid_state_hillary_voters = []
+    
+    solid_state_3rd_party_voters = []
+    swing_state_hillary_voters = []
+    
+
+   
+    #push into arrays
+    unpaired_voters.each do |voter|
+
+      if swing_states.include?( voter.user_state )
+        if voter.desired_candidate == 'hillary'
+          swing_state_hillary_voters << voter
+        else
+          swing_state_3rd_party_voters << voter
+        end
+      else 
+        if voter.desired_candidate == 'hillary'
+          solid_state_hillary_voters << voter
+        else
+          solid_state_3rd_party_voters << voter
+        end
+      end
+    end
+      
+    binding.pry
+
+    #create pairs ( swing-state-3rd-party + solid-state-hillary)
+    swing_state_3rd_party_voters.each do |voter|
+      # create pair hash
+
+      # put pair details into hash
+
+      #push hash into voter_pairs array
+
+      #make paired == true && remove from voters arrays (may need to use for loop)
+    end
+
+    #send email
+    #paired = true;
+
+    redirect_to '/'
   end
 
+  
 
 end
